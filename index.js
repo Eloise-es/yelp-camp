4351c099f450e4d4186e66bbcb162b4596155eca
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-02-08 14:18:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+// Require the modules
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+//Require the model
+
+// Settings
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+mongoose.set("strictQuery", false);
+
+// Mongoose connection to MongoDB
+main().catch((err) => console.log(err));
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("mongo connection open");
+}
