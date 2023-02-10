121335f06e0bf9907ac28508f0435153200e3ebc
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-02-10 17:44:12 +0100

    Add random images in seed data

--- seeds/index.js
+++ seeds/index.js
@@ -14,7 +14,14 @@ async function main() {
   console.log("mongo connection open");
 }
 
+// Define size of sample
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
+
+// Image generator
+const collectionID = 9046579; // the collection ID from the Unsplash url
+function renderImage(randomNumber) {
+  return `https://source.unsplash.com/collection/${collectionID}/480x400/?sig=${randomNumber}`;
+}
 
 // This will delete everything in the database, then replace it with 50 new campsites
 const seedDB = async () => {
@@ -22,10 +29,11 @@ const seedDB = async () => {
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 50);
+    const imgUrl = renderImage(Math.floor(Math.random() * 300));
     const camp = new Campsite({
       title: `${sample(descriptors)} ${sample(places)}`,
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
-      img: "https://picsum.photos/500/400",
+      img: imgUrl,
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt enim nostrum minus provident error, dolorum doloribus necessitatibus ab itaque quaerat corporis mollitia aspernatur cupiditate modi eos porro? Voluptatem, itaque neque!",
       price,
