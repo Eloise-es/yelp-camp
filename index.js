66bb4d6c5fc688e296320842060b4725a145aafd
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-02-28 09:27:41 +0100

    Moved form validation script to public directory

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const reviews = require("./routes/reviews");
 app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
