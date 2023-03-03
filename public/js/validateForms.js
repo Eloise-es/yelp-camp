c96f5873b999e7a671c7b1b6197444d0fb6196ab
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-03-03 16:40:42 +0100

    Added star ratings with starability

--- public/js/validateForms.js
+++ public/js/validateForms.js
@@ -20,3 +20,18 @@
     );
   });
 })();
+
+// To validate rating on review form
+const reviewForm = document.querySelector(".reviewForm");
+const defaultStarInput = document.querySelector("input[name='review[rating]']");
+const statusContainer = document.querySelector("#status");
+if (reviewForm) {
+  reviewForm.addEventListener("submit", function (e) {
+    if (defaultStarInput.checked) {
+      statusContainer.classList.remove("d-none");
+      e.preventDefault();
+    } else {
+      statusContainer.classList.add("d-none");
+    }
+  });
+}
