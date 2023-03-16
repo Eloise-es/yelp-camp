9cb1071677173d53363c7d24bcc67363587f58af
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-03-16 17:39:08 +0100

    remove local db url

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const ExpressError = require("./utils/ExpressError");
 
 // ENV VARIABLES
 const PORT = process.env.PORT || 3000;
-const dbUrl = process.env.DB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+const dbUrl = process.env.DB_URL; // || "mongodb://127.0.0.1:27017/yelp-camp";
 
 // Require the routes
 const userRoutes = require("./routes/users");
